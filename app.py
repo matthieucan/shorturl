1098fd8e83707709243c54b8671793e33f1cb73d
Matthieu Caneill <matthieu.caneill42@gmail.com>
2013-10-01 20:02:22 +0200

    add: errors/exceptions flow

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, redirect, url_for, request, render_template
 
-import data, url
+import data, url, excepts
 
 app = Flask(__name__)
 
@@ -8,15 +8,32 @@ app = Flask(__name__)
 def redir(encoded_url):
     try:
         url = data.retrieve(encoded_url)
+    except excepts.NotFoundException:
+        return notfound_error()
     except:
-        return(redirect(url_for("404")))
+        return undefined_error()
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
        if "url" in request.form:
-           encoded_url = url.encode(url) # encoding the url...
-           data.store(url, encoded_url)  # ... and storing it
-           
-
+           try:
+               encoded_url = url.encode(url) # encoding the url...
+               data.store(url, encoded_url)  # ... and storing it
+           except:
+               return undefined_error()
+    
     return render_template("index.html")
+
+# error handlers
+
+@app.errorhandler(500)
+def server_error():
+    return render_template("500.html")
+
+@app.errorhandler(404)
+def notfound_error():
+    return render_template("404.html")
+
+def undefined_error():
+    return render_template("undefined_error.html")

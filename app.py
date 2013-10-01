4e2e0828a2565cff8ca44cae7b193b90e77f81eb
Matthieu Caneill <matthieu.caneill42@gmail.com>
2013-10-01 19:46:18 +0200

    routing internals

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 from flask import Flask, redirect, url_for, request, render_template
 
+import data, url
+
 app = Flask(__name__)
 
-@app.route("/<url>")
-def redir(url):
+@app.route("/<encoded_url>")
+def redir(encoded_url):
     try:
-        return(redirect(decode(url)))
+        url = data.retrieve(encoded_url)
     except:
-        return(redirect(url_for("index")))
+        return(redirect(url_for("404")))
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    #if request.method == "POST":
-    #    if "url" in request.form:
-    #        return template(short=encode(request.form["url"]))
+    if request.method == "POST":
+       if "url" in request.form:
+           encoded_url = url.encode(url) # encoding the url...
+           data.store(url, encoded_url)  # ... and storing it
+           
 
     return render_template("index.html")

95abb43ab02dbcccea0a4efaf11bd3b9b2f1cfca
Matthieu Caneill <matthieu.caneill42@gmail.com>
2013-10-05 00:18:03 +0200

    add: models: 1st shot

---
+++ models.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, ForeignKey, UniqueConstraint
+from sqlalchemy import Integer, String, Index, Enum, LargeBinary
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+class Url(Base):
+    """ the table containing the urls, plus additional info """
+    __tablename__ = "urls"
+    
+    id = Column(Integer, primary_key=True)
+    # id is the number in base 10 which gives the short url in base 62
+    long_url = Column(String, unique=True)
+    
+    def __init__(self, long_url):
+        self.long_url = long_url
+        
+    def __repr__(self):
+        return self.id + ": " + self.long_url
+    

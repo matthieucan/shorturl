033ae1fd72e87dcba2a4d2a788e6c146ff295d4a
Matthieu Caneill <matthieu.caneill42@gmail.com>
2013-10-05 00:35:43 +0200

    move views (flask routing) into a separate file

--- app.py
+++ app.py
@@ -1,39 +1,5 @@
-from flask import Flask, redirect, url_for, request, render_template
-
-import data, excepts
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/<encoded_url>")
-def redir(encoded_url):
-    try:
-        url = data.retrieve(encoded_url)
-    except excepts.NotFoundException:
-        return notfound_error()
-    except:
-        return undefined_error()
-
-@app.route("/", methods=["GET", "POST"])
-def index():
-    if request.method == "POST":
-       if "url" in request.form:
-           try:
-               # encodes and stores the url:
-               encoded_url = data.store(url, encoded_url)
-           except:
-               return undefined_error()
-    
-    return render_template("index.html")
-
-# error handlers
-
-@app.errorhandler(500)
-def server_error():
-    return render_template("500.html")
-
-@app.errorhandler(404)
-def notfound_error():
-    return render_template("404.html")
-
-def undefined_error():
-    return render_template("undefined_error.html")
+import views

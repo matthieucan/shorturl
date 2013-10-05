d2fa995c6bdaddd157a3f7d56880648ed7e2f499
Matthieu Caneill <matthieu.caneill42@gmail.com>
2013-10-05 19:04:36 +0200

    models: add store, correct a bug in __repr__

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String
 
-from db import Base
+from db import Base, session
 
 class Url(Base):
     """ the table containing the urls, plus additional info """
@@ -13,7 +13,11 @@ class Url(Base):
     
     def __init__(self, long_url):
         self.long_url = long_url
+    
+    def store(self):
+        session.add(self)
+        session.commit()
         
     def __repr__(self):
-        return self.id + ": " + self.long_url
+        return str(self.id) + ": " + self.long_url
     

345560c72383958613f2168424b37a8afe63b188
Matthieu Caneill <matthieu.caneill42@gmail.com>
2013-10-06 17:02:21 +0200

    add: settings: server name

--- settings.py
+++ settings.py
@@ -2,4 +2,9 @@ import os
 
 app_folder = os.getcwd()
 
+# the database uri, db backend has to be compatible with SQLAlchemy
 DATABASE_URI = "sqlite:///" + os.path.join(app_folder, "data/db.sqlite")
+
+# short url prefix, normally the url of your server
+# should end with a slash
+SHORT_URL_PREFIX = "http://127.0.0.1:5000/"

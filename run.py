40b365a23ea5fe1ca7eaf2bbc4788d6149bdcf34
Matthieu Caneill <matthieu.caneill@savoirfairelinux.com>
2014-05-13 14:44:13 -0400

    add --ip, --port to run.py

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
+import argparse
+
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Run Flask app.')
+    parser.add_argument('--ip', type=str, default='127.0.0.1', required=False,
+                        help='Local IP adress')
+    parser.add_argument('--port', type=int, default=5000, required=False,
+                        help='Port in use')
+    args = parser.parse_args()
+    
     from app import app
-    app.run(debug=True)
+    app.run(debug=True, host=args.ip, port=args.port)
